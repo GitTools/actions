d41411db453cd24b40b644ee60ee877dfbe62930
Artur Stolear <arturcic@gmail.com>
2019-11-29 10:54:01 +0200

    GH-6 Support all GitVersion variables

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,36 @@ function getGitVersionOptions(buildAgent: IBuildAgent): IGitVersionOptions {
 }
 
 function writeGitVersionToAgent(buildAgent: IBuildAgent, gitversion: IGitVersion): void {
-    buildAgent.setOutput("fullSemVer", gitversion.FullSemVer);
-    buildAgent.setOutput("majorMinorPatch", gitversion.MajorMinorPatch);
-    buildAgent.setOutput("shortSha", gitversion.ShortSha);
+
+    buildAgent.setOutput("major",                           gitversion.Major.toString());
+    buildAgent.setOutput("minor",                           gitversion.Minor.toString());
+    buildAgent.setOutput("patch",                           gitversion.Patch.toString());
+    buildAgent.setOutput("preReleaseTag",                   gitversion.PreReleaseTag);
+    buildAgent.setOutput("preReleaseTagWithDash",           gitversion.PreReleaseTagWithDash);
+    buildAgent.setOutput("preReleaseLabel",                 gitversion.PreReleaseLabel);
+    buildAgent.setOutput("preReleaseNumber",                gitversion.PreReleaseNumber.toString());
+    buildAgent.setOutput("weightedPreReleaseNumber",        gitversion.WeightedPreReleaseNumber.toString());
+    buildAgent.setOutput("buildMetaData",                   gitversion.BuildMetaData.toString());
+    buildAgent.setOutput("buildMetaDataPadded",             gitversion.BuildMetaDataPadded);
+    buildAgent.setOutput("fullBuildMetaData",               gitversion.FullBuildMetaData);
+    buildAgent.setOutput("majorMinorPatch",                 gitversion.MajorMinorPatch);
+    buildAgent.setOutput("semVer",                          gitversion.SemVer);
+    buildAgent.setOutput("legacySemVer",                    gitversion.LegacySemVer);
+    buildAgent.setOutput("legacySemVerPadded",              gitversion.LegacySemVerPadded);
+    buildAgent.setOutput("assemblySemVer",                  gitversion.AssemblySemVer);
+    buildAgent.setOutput("assemblySemFileVer",              gitversion.AssemblySemFileVer);
+    buildAgent.setOutput("fullSemVer",                      gitversion.FullSemVer);
+    buildAgent.setOutput("informationalVersion",            gitversion.InformationalVersion);
+    buildAgent.setOutput("branchName",                      gitversion.BranchName);
+    buildAgent.setOutput("sha",                             gitversion.Sha);
+    buildAgent.setOutput("shortSha",                        gitversion.ShortSha);
+    buildAgent.setOutput("nuGetVersionV2",                  gitversion.NuGetVersionV2);
+    buildAgent.setOutput("nuGetVersion",                    gitversion.NuGetVersion);
+    buildAgent.setOutput("nuGetPreReleaseTagV2",            gitversion.NuGetPreReleaseTagV2);
+    buildAgent.setOutput("nuGetPreReleaseTag",              gitversion.NuGetPreReleaseTag);
+    buildAgent.setOutput("versionSourceSha",                gitversion.VersionSourceSha);
+    buildAgent.setOutput("commitsSinceVersionSource",       gitversion.CommitsSinceVersionSource.toString());
+    buildAgent.setOutput("commitsSinceVersionSourcePadded", gitversion.CommitsSinceVersionSourcePadded);
+    buildAgent.setOutput("commitDate",                      gitversion.CommitDate);
+
 }
