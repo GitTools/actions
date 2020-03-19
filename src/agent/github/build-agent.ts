627d0037578f748e433a6bd2c0b990246958b6a4
Artur Stolear <arturcic@gmail.com>
2020-03-19 12:23:04 +0200

    Bump uuid from 3.4.0 to 7.0.0

--- src/agent/github/build-agent.ts
+++ src/agent/github/build-agent.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-import uuidv4 from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 import * as core from '@actions/core'
 import * as exe from '@actions/exec'
