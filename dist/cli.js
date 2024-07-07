21d28bef80def7301c2197f57fe43e3610df3b5b
Artur Stolear <artur.stolear@gmail.com>
2024-07-07 12:45:51 +0200

    add main.js to dist/azure/*

--- dist/cli.js
+++
@@ -1,6 +0,0 @@
-import { parseCliArgs, getToolRunner } from './lib.js';
-
-const { buildAgent, tool, command } = parseCliArgs();
-const runner = await getToolRunner(buildAgent, tool);
-await runner.run(command);
-//# sourceMappingURL=cli.js.map
