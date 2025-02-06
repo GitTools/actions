d34f3810b7f712d0d4a393338affe29251b3e2e4
Artur Stolear <artur.stolear@gmail.com>
2025-02-06 05:45:08 +0000

    dist update

--- dist/tools/libs/agents.mjs
+++ dist/tools/libs/agents.mjs
@@ -6,14 +6,14 @@ import * as util from 'node:util';
 import { s as semverExports } from './semver.mjs';
 
 const isFilePath = (cmd) => {
-  return cmd.includes(path.sep) ? path.resolve(cmd) : void 0;
+  return cmd.includes(path.sep) ? path.resolve(cmd) : undefined;
 };
 const access = async (filePath) => {
   try {
     await fs.access(filePath);
     return filePath;
   } catch (_error) {
-    return void 0;
+    return undefined;
   }
 };
 const isExecutable = async (absPath, options = {}) => {
