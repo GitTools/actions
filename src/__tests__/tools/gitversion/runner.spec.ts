565948e5cbf7b87a4a79ab18a01532aa5e382c4a
SimplyAName <michael.loney137@gmail.com>
2025-10-14 22:11:25 +0100

    Attempt 2 to fix pipeline tests

--- src/__tests__/tools/gitversion/runner.spec.ts
+++ src/__tests__/tools/gitversion/runner.spec.ts
@@ -185,7 +185,7 @@ describe('GitVersion Runner', () => {
                 arguments: '/showvariable Sha'
             })
 
-            const sha = await simpleGit().revparse(['HEAD'])
+            const sha = await simpleGit().revparse()
             const result = await runner.run('command')
 
             expect(result.code).toBe(0)
@@ -200,7 +200,7 @@ describe('GitVersion Runner', () => {
                 arguments: '/format {Sha}'
             })
 
-            const sha = await simpleGit().revparse(['HEAD'])
+            const sha = await simpleGit().revparse()
             const result = await runner.run('command')
 
             expect(result.code).toBe(0)
